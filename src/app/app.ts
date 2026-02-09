a828c0c7b90704b207f4ad1b520d42da31950d8e
Luke Offerman <lukeofferman@yahoo.com>
2026-02-08 21:20:52 -0500

    Native Federation: remote added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('we-jay-guest');
 }
+
+export { App as AppComponent };
